8fbfa49e9f301e76b252e2ca7967094a00b16068
UnaiGuerraAzarquiel <alumno.659727@ies-azarquiel.es>
2025-04-02 21:41:58 +0200

    Test de paquete Unai

---
+++ src/main/java/Proyecto/Unai/Zapatilla.java
@@ -0,0 +1,10 @@
+package Proyecto.Unai;
+
+public class Zapatilla {
+    public static void main(String[] args) {
+        String zapatilla="zapatilla";
+        String hecatombe ="hecatombe";
+        System.out.println("hecatombe = " + hecatombe);
+        System.out.println("zapatilla = " + zapatilla);
+    }
+}
