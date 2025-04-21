026dcbbfcb0d0e23e0b251b2aaf5a4229913976c
Paaulaa29 <alumno.647654@ies-azarquiel.es>
2025-04-21 13:16:29 +0200

    Código (Con pocos comentarios) Main con las 3 formas de implementar una interfaz funcional (comparator) Libro, que es la clase con la que trabaja el main CompararTitulo, que es la clase explícita para implementar comparator

---
+++ src/main/java/Proyecto/Paula/Libro.java
@@ -0,0 +1,34 @@
+package Proyecto.Paula;
+
+public class Libro {
+    private String titulo=null;
+    private String autor=null;
+    private int año;
+
+    public Libro(String titulo, String autor, int año) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.año = año;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public int getAño() {
+        return año;
+    }
+
+    @Override
+    public String toString() {
+        return "Libro{" +
+                "titulo='" + titulo + '\'' +
+                ", autor='" + autor + '\'' +
+                ", año=" + año +
+                '}';
+    }
+}
