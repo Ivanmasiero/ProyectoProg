c6cd8aebcd804c7ab04edde3e3978452ec475f27
Diego-byte945 <alumno.560757@ies-azarquiel.es>
2025-04-27 12:27:32 +0200

    Parte del trabajo de Diego, añado la clase InterfazCollection a mi carpeta ya completa.

--- src/main/java/Proyecto/Diego/InterfazCollection.java
+++ src/main/java/Proyecto/Diego/InterfazCollection.java
@@ -32,10 +32,32 @@ public class InterfazCollection {
             String p= hola.next();
             System.out.println(p);
         }
-        //Usamos el metodo .clear para quitar todos los elementos de la colección
-        coleccion.clear();
-        System.out.println("Después de usar clear la colección contiene: "+coleccion);
-        //Vamos a comprobar con .isEmpty si la colección esta vacía
-        System.out.println("¿La coleccion está vacía? "+coleccion.isEmpty());
+        //Vamos a crear otra colección del mismo tipo para explicar los métodos que faltan.
+        ArrayList<String> coleccion2 = new ArrayList<>();
+        //Añadimos nuevos elementos a esta colección.
+        coleccion2.add("Ejemplo 4");
+        coleccion2.add("Ejemplo 5");
+        coleccion2.add("Ejemplo 6");
+        System.out.println("La colección2 contiene: "+coleccion2);
+        //En este caso usaremos el metodo constainsAll que lo que hace es que si todos los elementos de la
+        //segunda colección están en la primera devolverá true, si no es asi devuelve false.
+        System.out.println("La colección2 tiene los mismos elementos que colección: "+coleccion.containsAll(coleccion2));
+        //Ahora usaremos el metodo adAll que añade a la colección que hace la llamada todos los elementos
+        //de la colección a la que llama.
+        coleccion.addAll(coleccion2);
+        System.out.println("La colección ahora tiene estos elementos: "+coleccion);
+        //El próximo metodo es el removeAll que elimina de la colección que hace la llamada los elementos
+        //que están en la colección que has llamado.
+        coleccion.removeAll(coleccion2);
+        System.out.println("Después de hacer el borrado la colección contiene: "+coleccion);
+        //Otro metodo es el retainAll el cual elimina todos los elementos de la colección que hace la llamada
+        //excepto los que coincidan con los elementos que están en la colección llamada.
+        coleccion.retainAll(coleccion2);
+        System.out.println("La colección ahora contiene: "+coleccion);
+        //Usamos el metodo .clear con la colección2 para quitar todos los elementos de la colección.
+        coleccion2.clear();
+        System.out.println("Después de usar clear la colección2 contiene: "+coleccion2);
+        //Por último vamos a comprobar con .isEmpty si la colección esta vacía.
+        System.out.println("¿La colección2 está vacía? "+coleccion2.isEmpty());
     }
 }
