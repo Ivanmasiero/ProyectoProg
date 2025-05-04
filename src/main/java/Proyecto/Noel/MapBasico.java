856d79b2f07bec3bb981c84c4683b4557223cd05
noel <alumno.705088@ies-azarquiel.es>
2025-05-05 00:02:03 +0200

    Formateado
    Formateado

--- src/main/java/Proyecto/Noel/MapBasico.java
+++ src/main/java/Proyecto/Noel/MapBasico.java
@@ -1,5 +1,6 @@
 package Proyecto.Noel;
 
+import java.awt.print.Printable;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -27,37 +28,38 @@ public class MapBasico {
     public static void main(String[] args) {
         //Creamos un mapa con una clave tipo Integer y valores tipo String
         Map<Integer, String> empleados = new HashMap<>();
-
         //Insertamos varios registros
         empleados.put(1,"Juanjo");
         empleados.put(2,"Pepe");
         empleados.put(3,"Héctor");
 
         //Mostramos el contenido del mapa.
-        System.out.println(empleados); // Se muestra con llaves porque es un mapa, no una Collection
-        System.out.println(empleados.entrySet()); //Aquí, al transformarlo en un Set, ahora sí es Iterable.
+        System.out.println("\nPrint del mapa:\n" + empleados + "\n\n"); // Se muestra con llaves porque es un mapa, no una Collection
+        System.out.println("Print del Set del mapa:\n" + empleados.entrySet() + "\n\n"); //Aquí, al transformarlo en un Set, ahora sí es Iterable.
 
         //Sobreescribimos a Pepe
         empleados.put(2, "Manuel");
-        System.out.println(empleados);
+        System.out.println("Print del mapa con Manuel en vez de Pepe:\n" + empleados + "\n\n");
 
         //Eliminamos una entrada por su clave
         empleados.remove(1); //También se podría especificar el valor que tiene que tener
 
         //Mostramos el valor de una entrada por su clave
-        System.out.println(empleados.get(2)); // mostrar el valor de una clave
-        System.out.println(empleados.getOrDefault(5,"Valor por defecto")); //También se puede mostrar un valor por defecto si no existe la clave.
+        System.out.println("Print del valor dada una clave:\n" + empleados.get(2) + "\n\n"); // mostrar el valor de una clave
+        System.out.println("Print del valor (o default) dada una clave:\n" +empleados.getOrDefault(5,"Valor por defecto") + "\n\n"); //También se puede mostrar un valor por defecto si no existe la clave.
 
         //Comprobamos si existe alguna entrada con esta clave
-        System.out.println(empleados.containsKey(2)
+
+        System.out.println("vemos si existe la clave insertada:\n" +(empleados.containsKey(2)
                 ? "Sí hay un empleado con esta clave"
-                : "No hay un empleado con esta clave");
+                : "No hay un empleado con esta clave") + "\n\n");
 
         //Comprobamos si existe alguna entrada con este valor.
-        System.out.println(empleados.containsValue("Julio")
+        System.out.println("Vemos si existe el valor insertado:\n" + (empleados.containsValue("Julio")
                 ? "Sí hay un empleado con este valor"
-                : "No hay un empleado con este valor");
+                : "No hay un empleado con este valor") + "\n\n");
 
+        System.out.println("Bucle:");
         empleados.forEach((id, nombre) -> System.out.println(id + ": " + nombre));
 
     }
--- src/main/java/Proyecto/Noel/MapUsuario.java
+++ src/main/java/Proyecto/Noel/MapUsuario.java
@@ -22,27 +22,31 @@ public class MapUsuario {
 
         usuario = new Usuario("mario69","mariobros@example.com",925232081);
         users.put(usuario.getUsername(),usuario);
-        System.out.println(users);
+
+        System.out.println("\nImprimimos todos los usuarios:\n"+ users + "\n\n");
 
         //Intentamos registrar un usuario con un nombre ya existente
         usuario = new Usuario("ana26","anita@example.com", 925540955);
-        if(users.containsKey(usuario.getUsername())) {
-            System.out.println("El usuario "+ usuario.getUsername() +" ya está dado de alta.");
-        } else {
+
+        System.out.println("Intentamos añadir al usuario con el username " + usuario.getUsername() + ":");
+        if(users.containsKey(usuario.getUsername()))
+            System.out.println("El usuario "+ usuario.getUsername() +" ya está dado de alta.\n\n");
+        else {
             users.put(usuario.getUsername(),usuario);
-            System.out.println("Usuario " + usuario.getUsername() + " dado de alta con éxito.");
+            System.out.println("Usuario " + usuario.getUsername() + " dado de alta con éxito.\n\n");
         }
 
         //Comprobamos si un correo ya está registrado (con equals en Usuario).
         Usuario buscado = new Usuario("pacoimpostor","pacochulo@example.com", 696969696);
-        if(users.containsValue(buscado)) {
-            System.out.println("El usuario con el correo " + buscado.getEmail() + " existe. ");
-        } else {
-            System.out.println("El usuario con el correo " + buscado.getEmail() + " no existe. ");
-        }
+        System.out.println("Buscamos si existe el email de " + buscado.getEmail() + ":");
+
+        if(users.containsValue(buscado))
+            System.out.println("El usuario con el correo " + buscado.getEmail() + " existe.\n\n");
+        else
+            System.out.println("El usuario con el correo " + buscado.getEmail() + " no existe.\n\n");
 
         //Borramos una entrada por la clave
         users.remove("mario69");
-        System.out.println(users);
+        System.out.println("Tras borrar a mario, imprimimos usuarios:\n" + users + "\n\n");
     }
 }
