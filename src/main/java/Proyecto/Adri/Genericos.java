8859cf7787c142adc6a97af445884f3f26b2b45c
adrion279 <adriansanchez279@gmail.com>
2025-05-05 03:42:25 +0200

    ultimo retoque

--- src/main/java/Proyecto/Adri/Genericos.java
+++ src/main/java/Proyecto/Adri/Genericos.java
@@ -6,37 +6,37 @@ public class Genericos {
      con esto conseguimos reutilizar codigo y que una misma clase pueda trabajar con diferentes
      tipos de datos y esto evitamos muchos errores de compilación */
 
-    //He aqui un ejemplo de una clase generica
-    class Contenedor<T> {
+
+    public static class Contenedor<T> {
         private T objeto;
-
         public void guardar(T nuevo) { objeto = nuevo; }
-
         public T extraer() {
             T res = objeto;
             objeto = null;
             return res;
         }
         //Aqui usamos un comodin o wildcard, este sirve para que trabaje con todo tipo de datos
-        public static void verContenido(Contenedor<?> c) {
-            System.out.println("Contenido: " + c.objeto);
+        public static void verContenido(Contenedor<? extends Object> c) {
+            if (c.objeto != null) {
+                System.out.println("Contenido del contenedor de tipo " + c.objeto.getClass().getSimpleName() + ": " + c.objeto);
+            } else {
+                System.out.println("El contenedor está vacío.");
+            }
         }
     }
-    public void main(String[] args) {
+    public static void main(String[] args) {
         // Con <Integer> nos aseguramos que si o si el tipo de dato que se utilizara sera un entero
         Contenedor<Integer> c = new Contenedor<>();
         c.guardar(5);
+        Contenedor.verContenido(c);
         Integer mano=c.extraer();
 
         Contenedor<String> c2= new Contenedor<>();
         c2.guardar("Hola Mundo");
+        Contenedor.verContenido(c2);
         String Pantalla=c2.extraer();
         //como vemos la misma clase sirve para varios tipos de dato
 
-
-        //
-        Contenedor.verContenido(c);
-        Contenedor.verContenido(c2);
     }
 
 
--- src/main/java/Proyecto/Adri/TiposDeColecciones.java
+++ src/main/java/Proyecto/Adri/TiposDeColecciones.java
@@ -7,35 +7,29 @@ public class TiposDeColecciones {
          * Se utilizan las colecciones, ya que estas son dinamicas al contrario de los arrays
          * lo que lo hace más flexible*/
         //Aquí os muestro los 3 tipos fundamentales de colecciones
-
-
-
-        //List: Este permite duplicados, y ordenadada
+        //List: Este permite duplicados, y ordenada
+        //Set: Sin duplicados y sin orden
+        //Map: Aunque este no hereda de collection es muy importante, contiene una clave
 
         List<String> frutas = new ArrayList<>();
         frutas.add("Manzana");
         frutas.add("Platano");
         frutas.add("Manzana"); // Se permite repetir
+        System.out.println("Salida Coleccion tipo List:  " +frutas);
 
-        System.out.println(frutas); // [Manzana, Platano, Manzana]
-
-
-        //Set: Sin duplicados y sin orden
         Set<String> colores = new HashSet<>();
         colores.add("Rojo");
         colores.add("Azul");
         colores.add("Rojo"); // Como vemos el propio intellij detecta que están duplicados.
 
-        System.out.println(colores); // [Rojo, Azul] (el orden puede variar)
+        System.out.println("Salida Coleccion tipo Set:  " +colores); // [Rojo, Azul] (el orden puede variar)
 
-
-        //Map: Aunque este no hereda de collection es muy importante
         Map<Integer, String> personas = new HashMap<>();
         personas.put(1, "Ana");
         personas.put(2, "Luis");
         personas.put(1, "María"); // Remplaza "Ana" porque la clave 1 ya existe
 
-        System.out.println(personas); // {1=María, 2=Luis}
+        System.out.println("Salida Coleccion tipo Map:  " +personas); // {1=María, 2=Luis}
 
     }
 }
